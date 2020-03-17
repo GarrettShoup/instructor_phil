ae64fc8d27fc683531784419a006d1dfa199f5d6
Phil Krull <pkrull@codingdojo.com>
2020-03-17 08:40:52 -0500

    initial commit

---
+++ web_fun/dayOneAlgorithms.js
@@ -0,0 +1,30 @@
+
+
+// for(var i=0; i<10;i++) {
+//     console.log(i);
+//     i=i+3;
+// }
+// console.log(i);
+
+
+// var a = 20;
+// function abc(list) {
+//     a = 15;
+//     console.log(a);
+// }
+// console.log(a);
+
+// a=[[1,2,3],[5,10,20]];
+
+function sum(list) {
+    sum = list[0];
+
+    for(var i=0; i<list.length; i++) {
+        sum=sum+list[i];
+        console.log(sum);
+    }
+    return sum;
+}
+
+c=sum(([1,2,3]));
+console.log('c is:', c);

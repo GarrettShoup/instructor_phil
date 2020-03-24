5408d3100e32732149f478d42c61b1d6ddf1ac03
Phil Krull <pkrull@codingdojo.com>
2020-03-24 16:04:57 -0500

    adding pacman

---
+++ web_fun/javaScript/introToObjects.js
@@ -0,0 +1,20 @@
+// What is an object?
+// collection of data
+// stores data in a series of key:value pairs
+
+var person = {
+    "name": "Phil",
+    "location": "Bixby"
+};
+
+console.log(person["name"]);
+// => "Phil"
+
+person["title"] = "Instructor";
+console.log(person);
+
+var myArr = [];
+
+myArr.push(5)
+
+console.log(myArr);
